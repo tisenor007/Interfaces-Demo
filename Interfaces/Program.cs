4a61f58c8baea85f3c9107dacc92c55d7a5f9969
Trenton Isenor <ronesinotnert@gmail.com>
2021-11-10 09:53:28 -0400

    Initial commit

---
+++ Interfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an Interfaces Demo");
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
